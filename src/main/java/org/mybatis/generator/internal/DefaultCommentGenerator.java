0241ebb71f0489d837237b614ad5fd1b716dd905
dengxu <mail.deng@qq.com>
2019-01-11 17:50:36 +0800

    0000000000000

--- src/main/java/org/mybatis/generator/internal/DefaultCommentGenerator.java
+++ src/main/java/org/mybatis/generator/internal/DefaultCommentGenerator.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2006-2017 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2006-2017 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.mybatis.generator.internal;
 
@@ -55,7 +55,9 @@ public class DefaultCommentGenerator implements CommentGenerator {
 
     private boolean suppressAllComments;
 
-    /** If suppressAllComments is true, this option is ignored. */
+    /**
+     * If suppressAllComments is true, this option is ignored.
+     */
     private boolean addRemarkComments;
 
     private SimpleDateFormat dateFormat;
@@ -76,8 +78,7 @@ public class DefaultCommentGenerator implements CommentGenerator {
     /**
      * Adds a suitable comment to warn users that the element was generated, and when it was generated.
      *
-     * @param xmlElement
-     *            the xml element
+     * @param xmlElement the xml element
      */
     @Override
     public void addComment(XmlElement xmlElement) {
@@ -118,13 +119,13 @@ public class DefaultCommentGenerator implements CommentGenerator {
 
         suppressDate = isTrue(properties
                 .getProperty(PropertyRegistry.COMMENT_GENERATOR_SUPPRESS_DATE));
-        
+
         suppressAllComments = isTrue(properties
                 .getProperty(PropertyRegistry.COMMENT_GENERATOR_SUPPRESS_ALL_COMMENTS));
 
         addRemarkComments = isTrue(properties
                 .getProperty(PropertyRegistry.COMMENT_GENERATOR_ADD_REMARK_COMMENTS));
-        
+
         String dateFormatString = properties.getProperty(PropertyRegistry.COMMENT_GENERATOR_DATE_FORMAT);
         if (StringUtility.stringHasValue(dateFormatString)) {
             dateFormat = new SimpleDateFormat(dateFormatString);
@@ -135,13 +136,11 @@ public class DefaultCommentGenerator implements CommentGenerator {
      * This method adds the custom javadoc tag for. You may do nothing if you do not wish to include the Javadoc tag -
      * however, if you do not include the Javadoc tag then the Java merge capability of the eclipse plugin will break.
      *
-     * @param javaElement
-     *            the java element
-     * @param markAsDoNotDelete
-     *            the mark as do not delete
+     * @param javaElement       the java element
+     * @param markAsDoNotDelete the mark as do not delete
      */
     protected void addJavadocTag(JavaElement javaElement,
-            boolean markAsDoNotDelete) {
+                                 boolean markAsDoNotDelete) {
         javaElement.addJavaDocLine(" *"); //$NON-NLS-1$
         StringBuilder sb = new StringBuilder();
         sb.append(" * "); //$NON-NLS-1$
@@ -161,7 +160,7 @@ public class DefaultCommentGenerator implements CommentGenerator {
      * Returns a formated date string to include in the Javadoc tag
      * and XML comments. You may return null if you do not want the date in
      * these documentation elements.
-     * 
+     *
      * @return a string representing the current timestamp, or null
      */
     protected String getDateString() {
@@ -176,7 +175,7 @@ public class DefaultCommentGenerator implements CommentGenerator {
 
     @Override
     public void addClassComment(InnerClass innerClass,
-            IntrospectedTable introspectedTable) {
+                                IntrospectedTable introspectedTable) {
         if (suppressAllComments) {
             return;
         }
@@ -198,7 +197,7 @@ public class DefaultCommentGenerator implements CommentGenerator {
 
     @Override
     public void addClassComment(InnerClass innerClass,
-            IntrospectedTable introspectedTable, boolean markAsDoNotDelete) {
+                                IntrospectedTable introspectedTable, boolean markAsDoNotDelete) {
         if (suppressAllComments) {
             return;
         }
@@ -220,12 +219,12 @@ public class DefaultCommentGenerator implements CommentGenerator {
 
     @Override
     public void addModelClassComment(TopLevelClass topLevelClass,
-            IntrospectedTable introspectedTable) {
+                                     IntrospectedTable introspectedTable) {
 
         String comment = "@Entity(name = \"" + introspectedTable.getFullyQualifiedTable() + "\")";
         topLevelClass.addAnnotation(comment);
 
-        if (suppressAllComments  || !addRemarkComments) {
+        if (suppressAllComments || !addRemarkComments) {
             return;
         }
 
@@ -256,7 +255,7 @@ public class DefaultCommentGenerator implements CommentGenerator {
 
     @Override
     public void addEnumComment(InnerEnum innerEnum,
-            IntrospectedTable introspectedTable) {
+                               IntrospectedTable introspectedTable) {
         if (suppressAllComments) {
             return;
         }
@@ -278,16 +277,24 @@ public class DefaultCommentGenerator implements CommentGenerator {
 
     @Override
     public void addFieldComment(Field field,
-            IntrospectedTable introspectedTable,
-            IntrospectedColumn introspectedColumn) {
+                                IntrospectedTable introspectedTable,
+                                IntrospectedColumn introspectedColumn) {
 
-        String comment = "";
-        if(introspectedColumn.getActualColumnName().contains("Id")){
-            comment = "@Column(name=\"" + introspectedColumn.getActualColumnName() + "\", updatable=false)";
-        }else {
-            comment = "@Column(name=\"" + introspectedColumn.getActualColumnName() + "\")";
+
+
+        StringBuilder sbt = new StringBuilder();
+        if (introspectedColumn.getActualColumnName().contains("ID")) {
+            String generator = introspectedTable.getFullyQualifiedTable() + "_seq";
+            String sequenceName = introspectedTable.getFullyQualifiedTable() + "_id_seq";
+            sbt.append("\r\n");
+            sbt.append("    @Id").append("\r\n");
+            sbt.append("    @GeneratedValue(strategy= GenerationType.SEQUENCE, generator=\"" + generator + "\")").append("\r\n");
+            sbt.append("    @SequenceGenerator(name=\"" + generator + "\", sequenceName=\"" + sequenceName + "\",initialValue = 1 , allocationSize = 1)").append("\r\n");
+            sbt.append("    @Column(name=\"" + introspectedColumn.getActualColumnName() + "\", updatable=false)");
+        } else {
+            sbt.append("@Column(name=\"" + introspectedColumn.getActualColumnName() + "\")");
         }
-        field.addAnnotation(comment);
+        field.addAnnotation(sbt.toString());
 
         if (suppressAllComments) {
             return;
@@ -343,7 +350,7 @@ public class DefaultCommentGenerator implements CommentGenerator {
 
     @Override
     public void addGeneralMethodComment(Method method,
-            IntrospectedTable introspectedTable) {
+                                        IntrospectedTable introspectedTable) {
         if (suppressAllComments) {
             return;
         }
@@ -365,8 +372,8 @@ public class DefaultCommentGenerator implements CommentGenerator {
 
     @Override
     public void addGetterComment(Method method,
-            IntrospectedTable introspectedTable,
-            IntrospectedColumn introspectedColumn) {
+                                 IntrospectedTable introspectedTable,
+                                 IntrospectedColumn introspectedColumn) {
         if (suppressAllComments) {
             return;
         }
@@ -399,8 +406,8 @@ public class DefaultCommentGenerator implements CommentGenerator {
 
     @Override
     public void addSetterComment(Method method,
-            IntrospectedTable introspectedTable,
-            IntrospectedColumn introspectedColumn) {
+                                 IntrospectedTable introspectedTable,
+                                 IntrospectedColumn introspectedColumn) {
         if (suppressAllComments) {
             return;
         }
@@ -436,7 +443,7 @@ public class DefaultCommentGenerator implements CommentGenerator {
 
     @Override
     public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable,
-            Set<FullyQualifiedJavaType> imports) {
+                                           Set<FullyQualifiedJavaType> imports) {
         imports.add(new FullyQualifiedJavaType("javax.annotation.Generated")); //$NON-NLS-1$
         String comment = "Source Table: " + introspectedTable.getFullyQualifiedTable().toString(); //$NON-NLS-1$
         method.addAnnotation(getGeneratedAnnotation(comment));
@@ -444,7 +451,7 @@ public class DefaultCommentGenerator implements CommentGenerator {
 
     @Override
     public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable,
-            IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> imports) {
+                                           IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> imports) {
         imports.add(new FullyQualifiedJavaType("javax.annotation.Generated")); //$NON-NLS-1$
         String comment = "Source field: " //$NON-NLS-1$
                 + introspectedTable.getFullyQualifiedTable().toString()
@@ -455,7 +462,7 @@ public class DefaultCommentGenerator implements CommentGenerator {
 
     @Override
     public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable,
-            Set<FullyQualifiedJavaType> imports) {
+                                   Set<FullyQualifiedJavaType> imports) {
         imports.add(new FullyQualifiedJavaType("javax.annotation.Generated")); //$NON-NLS-1$
         String comment = "Source Table: " + introspectedTable.getFullyQualifiedTable().toString(); //$NON-NLS-1$
         field.addAnnotation(getGeneratedAnnotation(comment));
@@ -463,7 +470,7 @@ public class DefaultCommentGenerator implements CommentGenerator {
 
     @Override
     public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable,
-            IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> imports) {
+                                   IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> imports) {
         imports.add(new FullyQualifiedJavaType("javax.annotation.Generated")); //$NON-NLS-1$
         String comment = "Source field: " //$NON-NLS-1$
                 + introspectedTable.getFullyQualifiedTable().toString()
@@ -474,12 +481,12 @@ public class DefaultCommentGenerator implements CommentGenerator {
 
     @Override
     public void addClassAnnotation(InnerClass innerClass, IntrospectedTable introspectedTable,
-            Set<FullyQualifiedJavaType> imports) {
+                                   Set<FullyQualifiedJavaType> imports) {
         imports.add(new FullyQualifiedJavaType("javax.annotation.Generated")); //$NON-NLS-1$
         String comment = "Source Table: " + introspectedTable.getFullyQualifiedTable().toString(); //$NON-NLS-1$
         innerClass.addAnnotation(getGeneratedAnnotation(comment));
     }
-    
+
     private String getGeneratedAnnotation(String comment) {
         StringBuilder buffer = new StringBuilder();
         buffer.append("@Generated("); //$NON-NLS-1$
@@ -488,22 +495,22 @@ public class DefaultCommentGenerator implements CommentGenerator {
         } else {
             buffer.append("value=\""); //$NON-NLS-1$
         }
-        
+
         buffer.append(MyBatisGenerator.class.getName());
         buffer.append('\"');
-        
+
         if (!suppressDate && !suppressAllComments) {
             buffer.append(", date=\""); //$NON-NLS-1$
             buffer.append(DatatypeConverter.printDateTime(Calendar.getInstance()));
             buffer.append('\"');
         }
-        
+
         if (!suppressAllComments) {
             buffer.append(", comments=\""); //$NON-NLS-1$
             buffer.append(comment);
             buffer.append('\"');
         }
-        
+
         buffer.append(')');
         return buffer.toString();
     }
